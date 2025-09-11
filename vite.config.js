e61b1e787809610e75f3517f8fcfd28c7edc9e6a
Alberth1124 <alberthsibautylabardenz@gmail.com>
2025-09-10 23:31:45 -0400

    cargado1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
-});
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-dom/client']
+  },
+  resolve: {
+    alias: {
+      'react-dom/client': 'react-dom/client'
+    }
+  }
+})
